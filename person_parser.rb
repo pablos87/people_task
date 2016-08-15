3d44f02da203719d51fcb8b3efb55f4e21cf7618
lunatyq <maciej@lslabs.eu>
2016-08-15 10:02:59 +0200

    Parser suggestions

--- person_parser.rb
+++ person_parser.rb
@@ -1,15 +1,21 @@
-module PersonParser
-  def parse file_path
-    raw_people = []
-    begin
+class PersonParser
+  # vattr_initialize :csv_file_path - requires attr_extras, lub
+  # static_facade :run, :csv_file_path - ^^ + PersonParser.run(csv_file_path) => [#<Person>, ...]
+  attr_accessor :csv_file_path
+
+  def initialize(csv_file_path)
+    self.csv_file_path = csv_file_path
+  end
+
+  def people
+    raw_people.map { |raw_person| Person.new(raw_person) }
+  end
+
+  def raw_people
+    @raw_people ||= [].tap do |raw_people|
       CSV.foreach(file_path, :headers => true) do |raw_person|
-        raw_people << { name: raw_person[0], last_name: raw_person[1] }
+        raw_people << { name: raw_person['name'], last_name: raw_person['lastname'] }
       end
-    rescue Errno::ENOENT => e
-      puts "ArgumentError: #{e.message}"
-      exit
-    else
-      raw_people
     end
   end
 end
